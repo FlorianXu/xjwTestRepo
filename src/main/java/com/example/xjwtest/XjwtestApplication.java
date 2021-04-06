06ef89e337340ad71454344ade04d500a867287a
xjw4482 <xy970825@gmail.com>
2021-04-06 11:16:50 +0800

    commit到localrepo

---
+++ src/main/java/com/example/xjwtest/XjwtestApplication.java
@@ -0,0 +1,13 @@
+package com.example.xjwtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XjwtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XjwtestApplication.class, args);
+    }
+
+}
